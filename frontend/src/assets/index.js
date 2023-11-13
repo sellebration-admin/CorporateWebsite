87559ee01508899d02c1d5e353339c4523652ace
mironovisa <mironovisa@gmail.com>
2023-11-13 19:50:41 +0200

    Homepage finished

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,13 +1,26 @@
 import map from "./image.png";
-import logo from "./logo.png";
+import logo from "./logofor.png";
 import sortIcon from "./Sort.svg";
 import search from "./search.png";
 import news from "./news.jpg";
 import twitterIcon from "./twitter.png";
 import linkedinIcon from "./linkedin.png";
-import twitterFooter from "./twitterfooter.png";
+import twitterFooter from "./twitterfooterthree.png";
 import linkedinFooter from "./linkedinFooter.png";
 import instagramFooter from "./instagramfooter.png";
 import facebookFooter from "./facebookFooter.png";
 import youtubeFooter from "./youtubeFooter.png";
-export { map, logo, sortIcon, search, news, twitterIcon, linkedinIcon, twitterFooter, linkedinFooter, instagramFooter, facebookFooter, youtubeFooter};
+export {
+  map,
+  logo,
+  sortIcon,
+  search,
+  news,
+  twitterIcon,
+  linkedinIcon,
+  twitterFooter,
+  linkedinFooter,
+  instagramFooter,
+  facebookFooter,
+  youtubeFooter,
+};
